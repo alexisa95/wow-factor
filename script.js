ae3e641c8cbfefaa8634f9e033f9638a39929ba2
Alexis <lexisadams92@gmail.com>
2021-03-28 20:05:45 -0400

    added scipt.js, connected to index.html

---
+++ script.js
@@ -0,0 +1,11 @@
+let h2 = document.createElement('h2')
+h2.textContent = "Your everyday package selector turned software developer!"
+
+document.querySelector('body').appendChild(h2)
+
+const input = document.getElementById('input');
+
+document.getElementById("myBtn").addEventListener("click", function() {
+    alert("Thankyou for visiting my site!!!");
+  });
+
